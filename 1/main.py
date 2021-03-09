a96966f66af239dc12d7da3a4f01f719bc0c1cd7
Jakub Stefko <jakub.stefko@intive.com>
2021-03-09 18:10:15 +0100

    1.5 done

--- 1/main.py
+++ 1/main.py
@@ -1,4 +1,10 @@
+ 
 def main():
-    print("Hej, co tam?\n")
+    file_name = input("Enter file name:\t")
+    word = input("Enter word:\t")
+
+    with open(file_name) as file:
+        if word in file.read():
+            print("I don\'t like snakes but the quest is done and the word is found")
 
 main()
